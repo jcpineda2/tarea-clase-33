50bb671ee08f722e01422030681691a37c75aa96
jpineda <jpineda@senatur.gov.py>
2026-05-09 22:47:56 -0300

    feat: implementar axios

--- src/hooks/usePeticion.js
+++ src/hooks/usePeticion.js
@@ -1,16 +1,24 @@
 import datosPlaceHolder from"./../cache/pagina1.json";
+import Axios from "axios"
+
 
 export const usePeticion = async ({
     url,
     metodo
 }) => {
     try {
-        const datos = await fetch(url, {
-            method: metodo
-        })
-        const res = await datos.json()
+        // Con fetch
+        // const datos = await fetch(url, {
+        //     method: metodo
+        // })
+        // const res = await datos.json()
 
-        return res
+        // return res
+        // Con axios
+        if(metodo==="GET"){
+            const peticion = await Axios.get(url)
+            return peticion.data
+        }
     } catch (error) {
         console.log("Luego, veremos");
         return datosPlaceHolder
